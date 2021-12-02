81a96e21e883aa05ac114e818d1531e8b9bc04fc
DatBoiii <90166694+DatBoiii@users.noreply.github.com>
2021-12-02 17:48:04 -0500

    first commit

---
+++ src/main/java/com/techtalentsouth/techtalentblog/BlogPosts/BlogPostRepository.java
@@ -0,0 +1,8 @@
+package com.techtalentsouth.techtalentblog.BlogPosts;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+
+}
+
